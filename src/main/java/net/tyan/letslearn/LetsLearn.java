88466d5d2041bf4e61785be597df84f9db3b094d
Tyan <tyan@mail.de>
2015-04-05 14:36:23 +0200

    Path change

---
+++ src/main/java/net/tyan/letslearn/LetsLearn.java
@@ -0,0 +1,31 @@
+package net.tyan.letslearn;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+/**
+ * by Tyan on 05.04.2015.
+ */
+
+@Mod(modid = "LetsLearn", name = "Lets Learn", version = "1.8-1.0")
+public class LetsLearn {
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+        
+    }
+
+
+}
