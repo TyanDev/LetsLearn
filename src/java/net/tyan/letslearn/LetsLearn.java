727dbe20fb35baca3229c7c68204cc50c575ad60
Tyan <tyan@mail.de>
2015-04-05 14:25:47 +0200

    Inital Commit

---
+++ src/java/net/tyan/letslearn/LetsLearn.java
@@ -0,0 +1,14 @@
+package net.tyan.letslearn;
+
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * by Tyan on 05.04.2015.
+ */
+
+@Mod(modid = "LetsLearn", name = "Lets Learn", version = "1.8-1.0")
+public class LetsLearn {
+
+    
+
+}
